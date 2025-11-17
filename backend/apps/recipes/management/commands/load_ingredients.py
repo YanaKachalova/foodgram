82eff7921df71613862c2ff91fe5b612cafc8237
YanaKachalova <yvk4x2@gmail.com>
2025-11-17 17:54:47 +0300

    Исправила замечания в файле load_ingredients.py.

--- backend/apps/recipes/management/commands/load_ingredients.py
+++ backend/apps/recipes/management/commands/load_ingredients.py
@@ -5,26 +5,28 @@ from apps.recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = "Загрузка ингредиентов из CSV"
+    help = 'Загрузка ингредиентов из CSV'
 
     def add_arguments(self, parser):
-        parser.add_argument("--path", default="data/ingredients.csv")
+        parser.add_argument('--path', default='data/ingredients.csv')
 
     def handle(self, *args, **opts):
-        path = opts["path"]
+        path = opts['path']
         created = skipped = 0
         try:
-            with open(path, encoding="utf-8") as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    _, is_created = Ingredient.objects.get_or_create(
-                        name=row["name"].strip(),
-                        measurement_unit=row["measurement_unit"].strip(),
-                    )
-                    created += int(is_created)
-                    skipped += int(not is_created)
-        except FileNotFoundError:
-            raise CommandError(f"Файл не найден: {path}")
+            file = open(path, encoding='utf-8')
+        except FileNotFoundError as err:
+            raise CommandError(f'Файл не найден: {path}') from err
+
+        with file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                _, is_created = Ingredient.objects.get_or_create(
+                    name=row['name'].strip(),
+                    measurement_unit=row['measurement_unit'].strip(),)
+                created += int(is_created)
+                skipped += int(not is_created)
+
         self.stdout.write(self.style.SUCCESS(
-            f"Ингредиенты: создано={created}, пропущено={skipped}"
+            f'Ингредиенты: создано={created}, пропущено={skipped}'
         ))
