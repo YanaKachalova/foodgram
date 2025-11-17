8008422a2a644c1c02822f57fbe48161339e020d
YanaKachalova <yvk4x2@gmail.com>
2025-11-18 02:17:23 +0300

    Сериализаторы из users перенесены в api.

--- backend/apps/api/serializers.py
+++ backend/apps/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 from apps.recipes.models import (Tag,
                                  Ingredient,
@@ -7,7 +9,7 @@ from apps.recipes.models import (Tag,
                                  RecipeIngredient,
                                  Favorite,
                                  ShoppingCart)
-from apps.users.serializers import UserReadSerializer
+from apps.users.models import User, Follow
 from .fields import Base64ImageField
 
 
@@ -226,3 +228,83 @@ class ShoppingCartSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = self.context['request'].user
         return ShoppingCart.objects.create(user=user, **validated_data)
+
+
+class UserReadSerializer(BaseUserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta(BaseUserSerializer.Meta):
+        model = User
+        fields = ('id',
+                  'email',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'avatar',
+                  'is_subscribed')
+
+    def get_is_subscribed(self, author):
+        request = self.context.get('request')
+        user = getattr(request, 'user', None)
+        if user and user.is_authenticated:
+            return author.following.filter(user_id=user.pk).exists()
+        return False
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = ('email',
+                  'username',
+                  'password',
+                  'first_name',
+                  'last_name')
+
+
+class AvatarSerializer(serializers.ModelSerializer):
+    avatar = Base64ImageField(required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = ('avatar',)
+
+
+class FollowReadSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='author.id')
+    email = serializers.ReadOnlyField(source='author.email')
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
+    avatar = serializers.ImageField(source='author.avatar', read_only=True)
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.IntegerField(
+        source='author.recipes.count',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('id',
+                  'email',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'avatar',
+                  'recipes',
+                  'recipes_count',
+                  )
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        recipes_limit = None
+        if request is not None:
+            recipes_limit = request.query_params.get('recipes_limit')
+
+        queryset = obj.author.recipes.all()
+        if recipes_limit is not None:
+            try:
+                queryset = queryset[:int(recipes_limit)]
+            except (TypeError, ValueError):
+                pass
+
+        return RecipeShortSerializer(queryset, many=True).data
--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,86 +1,87 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
-from djoser.serializers import UserSerializer as BaseUserSerializer
-from rest_framework import serializers
+# from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+# from djoser.serializers import UserSerializer as BaseUserSerializer
+# from rest_framework import serializers
 
-from apps.api.fields import Base64ImageField
-from .models import User, Follow
-
-
-class UserReadSerializer(BaseUserSerializer):
-    is_subscribed = serializers.SerializerMethodField()
-
-    class Meta(BaseUserSerializer.Meta):
-        model = User
-        fields = ('id',
-                  'email',
-                  'username',
-                  'first_name',
-                  'last_name',
-                  'avatar',
-                  'is_subscribed')
-
-    def get_is_subscribed(self, obj):
-        request = self.context.get('request')
-        if request and request.user.is_authenticated:
-            return obj.following.filter(user=request.user).exists()
-        return False
+# from apps.api.fields import Base64ImageField
+# from .models import User, Follow
 
 
-class UserCreateSerializer(BaseUserCreateSerializer):
-    class Meta(BaseUserCreateSerializer.Meta):
-        model = User
-        fields = ('email',
-                  'username',
-                  'password',
-                  'first_name',
-                  'last_name')
+# class UserReadSerializer(BaseUserSerializer):
+#     is_subscribed = serializers.SerializerMethodField()
+
+#     class Meta(BaseUserSerializer.Meta):
+#         model = User
+#         fields = ('id',
+#                   'email',
+#                   'username',
+#                   'first_name',
+#                   'last_name',
+#                   'avatar',
+#                   'is_subscribed')
+
+#     def get_is_subscribed(self, author):
+#         request = self.context.get('request')
+#         user = getattr(request, 'user', None)
+#         if user and user.is_authenticated:
+#             return author.following.filter(user_id=user.pk).exists()
+#         return False
 
 
-class AvatarSerializer(serializers.ModelSerializer):
-    avatar = Base64ImageField(required=False, allow_null=True)
+# class UserCreateSerializer(BaseUserCreateSerializer):
+#     class Meta(BaseUserCreateSerializer.Meta):
+#         model = User
+#         fields = ('email',
+#                   'username',
+#                   'password',
+#                   'first_name',
+#                   'last_name')
 
-    class Meta:
-        model = User
-        fields = ('avatar',)
+
+# class AvatarSerializer(serializers.ModelSerializer):
+#     avatar = Base64ImageField(required=False, allow_null=True)
+
+#     class Meta:
+#         model = User
+#         fields = ('avatar',)
 
 
-class FollowReadSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='author.id')
-    email = serializers.ReadOnlyField(source='author.email')
-    username = serializers.ReadOnlyField(source='author.username')
-    first_name = serializers.ReadOnlyField(source='author.first_name')
-    last_name = serializers.ReadOnlyField(source='author.last_name')
-    avatar = serializers.ImageField(source='author.avatar', read_only=True)
-    recipes = serializers.SerializerMethodField()
-    recipes_count = serializers.IntegerField(
-        source='author.recipes.count',
-        read_only=True,
-    )
+# class FollowReadSerializer(serializers.ModelSerializer):
+#     id = serializers.ReadOnlyField(source='author.id')
+#     email = serializers.ReadOnlyField(source='author.email')
+#     username = serializers.ReadOnlyField(source='author.username')
+#     first_name = serializers.ReadOnlyField(source='author.first_name')
+#     last_name = serializers.ReadOnlyField(source='author.last_name')
+#     avatar = serializers.ImageField(source='author.avatar', read_only=True)
+#     recipes = serializers.SerializerMethodField()
+#     recipes_count = serializers.IntegerField(
+#         source='author.recipes.count',
+#         read_only=True,
+#     )
 
-    class Meta:
-        model = Follow
-        fields = ('id',
-                  'email',
-                  'username',
-                  'first_name',
-                  'last_name',
-                  'avatar',
-                  'recipes',
-                  'recipes_count',
-                  )
+#     class Meta:
+#         model = Follow
+#         fields = ('id',
+#                   'email',
+#                   'username',
+#                   'first_name',
+#                   'last_name',
+#                   'avatar',
+#                   'recipes',
+#                   'recipes_count',
+#                   )
 
-    def get_recipes(self, obj):
-        from apps.api.serializers import RecipeShortSerializer
-        request = self.context.get('request')
-        recipes_limit = None
-        if request is not None:
-            recipes_limit = request.query_params.get('recipes_limit')
+#     def get_recipes(self, obj):
+#         from apps.api.serializers import RecipeShortSerializer
+#         request = self.context.get('request')
+#         recipes_limit = None
+#         if request is not None:
+#             recipes_limit = request.query_params.get('recipes_limit')
 
-        queryset = obj.author.recipes.all()
-        if recipes_limit is not None:
-            try:
-                queryset = queryset[:int(recipes_limit)]
-            except (TypeError, ValueError):
-                pass
+#         queryset = obj.author.recipes.all()
+#         if recipes_limit is not None:
+#             try:
+#                 queryset = queryset[:int(recipes_limit)]
+#             except (TypeError, ValueError):
+#                 pass
 
-        return RecipeShortSerializer(queryset, many=True).data
+#         return RecipeShortSerializer(queryset, many=True).data
