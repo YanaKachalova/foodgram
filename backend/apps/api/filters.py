d179382a8e096e6ab0e039ad54de84d64eaa6429
YanaKachalova <yvk4x2@gmail.com>
2025-11-18 01:26:13 +0300

    Внесены исправления.

--- backend/apps/api/filters.py
+++ backend/apps/api/filters.py
@@ -5,9 +5,10 @@ from apps.recipes.models import Recipe, Ingredient
 
 class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(field_name='is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart')
+    is_favorited = filters.NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.filters.NumberFilter(
+        method='filter_is_in_cart')
+
 
     class Meta:
         model = Recipe
