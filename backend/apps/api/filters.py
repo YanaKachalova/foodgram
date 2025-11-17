bbc1cdeb5bc4eb652dd9094ac2616f2c9f28651d
YanaKachalova <yvk4x2@gmail.com>
2025-11-17 21:38:31 +0300

    Внесены исправления.

--- backend/apps/api/filters.py
+++ backend/apps/api/filters.py
@@ -8,7 +8,6 @@ class RecipeFilter(filters.FilterSet):
     is_favorited = filters.NumberFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.NumberFilter(method='filter_is_in_cart')
 
-
     class Meta:
         model = Recipe
         fields = ('author',
